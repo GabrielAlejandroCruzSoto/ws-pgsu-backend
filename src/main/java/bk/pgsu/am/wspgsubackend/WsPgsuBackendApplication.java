6c546cd86ab391f166eb0a280354dc3281c02146
GabrielCruzSoto <Gabrielcrsoto@outlook.com>
2020-07-29 23:11:11 -0400

    proyect init

---
+++ src/main/java/bk/pgsu/am/wspgsubackend/WsPgsuBackendApplication.java
@@ -0,0 +1,13 @@
+package bk.pgsu.am.wspgsubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPgsuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPgsuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bk/pgsu/am/wspgsubackend/WsPgsuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bk.pgsu.am.wspgsubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsPgsuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
